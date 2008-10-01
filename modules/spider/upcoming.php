a1b89feb600135c7f5c975ae7840b0a07df9627a
Lewis Zhang <lewis_zhang@academicsuperstore.com>
2008-09-30 20:09:04 -0500

    add upcoming raw import

---
+++ modules/spider/upcoming.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+include('../../config/application.php');
+
+$rawRss = new RawRss();
+$rawRss->setRawRssData(file_get_contents('http://upcoming.yahoo.com/syndicate/v2/search_all/?loc=Austin&rt=1'));
+$rawRss->setSourceId(1);
+$rawRss->setDateCreated(date('Y-m-d H:i:s', CURRENT_TIMESTAMP));
+$rawRss->save();
+?>
