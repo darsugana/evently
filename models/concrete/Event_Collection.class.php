dd15e5beb607379e4bc5989277750b748a8a985e
root <root@pascal>
2009-02-23 01:44:48 -0600

    display results in a meaningful order, discard useless results

--- models/concrete/Event_Collection.class.php
+++ models/concrete/Event_Collection.class.php
@@ -1,12 +1,11 @@
 <?php
-
 /**
 * This is the starter class for Event_Collection_Generated.
  *
  * @see Event_Collection_Generated, CoughCollection
  **/
-class Event_Collection extends Event_Collection_Generated {
-	
+class Event_Collection extends Event_Collection_Generated
+{
 	public function loadBySearchString($searchString)
 	{
 		$search = new Ev_Search('events_all');
@@ -14,14 +13,18 @@ class Event_Collection extends Event_Collection_Generated {
 		if (is_array($result['matches']))
 		{
 			$eventIds = array_keys($result['matches']);
+			$db = Event::getDb();
 			$sql = Event::getLoadSql();
 			$sql .= '
 				WHERE
-					event.event_id IN (' . implode(', ', $eventIds)  . ')
+					`event`.`event_id` IN (' . implode(', ', $eventIds)  . ')
+					AND `event`.`date` >= ' . $db->quote(date('Y-m-d', time())) . '
+					AND `event`.`is_deleted` = 0
+				ORDER BY
+					`event`.`date`
 			';
 			$this->loadBySql($sql);
 		}
 	}
 }
 
-?>
