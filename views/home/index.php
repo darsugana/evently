b4081b5da02ff52c5195bd1c5eb5e148f34e064d
Lewis Zhang <lewis_zhang@academicsuperstore.com>
2009-03-22 06:30:52 -0500

    change up look

--- views/home/index.php
+++ views/home/index.php
@@ -1,5 +1,15 @@
-<h1>evently</h1>
+<h1><span>Evently</span></h1>
 <form class="search" action="/search/" method="get">
-	<input class="q" name="q" type="text" />
-	<input class="submit" type="submit" value="search" />
+	<fieldset>
+		<input class="q" name="q" type="text" />
+	</fieldset>
+	<fieldset>
+		<input class="submit" type="submit" value="Search Events" />
+		<span class="examples">
+			examples:
+			<a href="#">wine tasting</a>,
+			<a href="#">punk rock</a>,
+			<a href="#">kanye west</a>
+		</span>
+	</fieldset>
 </form>
