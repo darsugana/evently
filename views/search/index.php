c6af33a1a6bc8fea9373e28188c53684603e970b
Lewis Zhang <lewis_zhang@academicsuperstore.com>
2009-03-30 04:33:24 -0500

    new search results page

--- views/search/index.php
+++ views/search/index.php
@@ -1,5 +1,5 @@
 <h2>
-	<a href="/"><img src="/images/logo_small.gif" /></a>
+	<a href="/"><img src="/images/logo_small.gif" /></a> / Austin
 </h2>
 
 <form class="search" action="/search/" method="get">
@@ -7,8 +7,8 @@
 		<input class="q" name="q" type="text" value="<?php echo htmlentities($query) ?>" />
 	</fieldset>
 	<fieldset>
-		<span class="examples">found <?php echo $numEvents ?> events!</span>
 		<input class="submit" type="submit" value="Find Events" />
+		<span class="examples">found <?php echo $numEvents ?> events!</span>
 	</fieldset>
 </form>
 
@@ -47,9 +47,16 @@ if (count($eventsByDate))
 									<span class="time"><?php echo htmlentities(date('g:ia', strtotime($event->getDate()))) ?></span>
 									<a href="<?php echo $event->getLink() ?>"><?php echo htmlentities(Ev_String::wordTrim($event->getName(), 75), ENT_COMPAT, 'UTF-8', false) ?></a>
 								</h3>
-								<h4 class="link"><a href="<?php echo $event->getLink() ?>"><?php echo htmlentities($event->getLink()) ?></a></h4>
+								<h4 class="link"><a class="external" href="<?php echo $event->getLink() ?>"><?php echo htmlentities($event->getLink()) ?></a></h4>
 								<p class="description">
-									<?php echo htmlentities(Ev_String::wordTrim($event->getDescription())) ?>
+									<?php echo htmlentities(Ev_String::wordTrim(str_replace("\n", ' ', $event->getDescription()))) ?>
+								</p>
+								<p class="controls">
+									<a class="like" href="#">I like this</a>
+									<a class="attend" href="#">I'm going!</a>
+									<a class="comment" href="#">Comment</a>
+									<a class="report" href="#">X</a>
+									<div class="clear"></div>
 								</p>
 							</li>
 							
@@ -57,6 +64,9 @@ if (count($eventsByDate))
 						}
 						?>
 					</ul>
+				</td>
+				<td class="sidebar">
+					
 				</td>
 			</tr>
 			
