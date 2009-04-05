64f37e5459552bf26ac8c3411f61e6e4ec984396
Lewis Zhang <lewis_zhang@academicsuperstore.com>
2009-04-04 21:33:03 -0500

    tar up unrelated sphinx  project files

--- modules/sphinx/src/sphinxversion.h
+++
@@ -1,4 +0,0 @@
-#define SPH_SVN_TAG ""
-#define SPH_SVN_REV 1566
-#define SPH_SVN_REVSTR "1566"
-#define SPH_SVN_TAGREV "r1566"
--- modules/sphinx/src/yy.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-bison -l -d -o sphinxexpryy.cpp sphinxexpr.y
-bison -l -d -o sphinxselectyy.cpp sphinxselect.y
-bison -l -d -o sphinxqueryyy.cpp sphinxquery.y
